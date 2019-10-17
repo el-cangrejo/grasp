3fbe6d4e58354d38babc980e5ceaaeef5c19d82b
dimid <mijuomij@gmail.com>
2019-10-17 16:46:18 +0100

    Request hand pose, read grasp index and save to yml file

--- examples/record_poses.cc
+++ examples/record_poses.cc
@@ -1,5 +1,9 @@
 #include "record_poses.hh"
 
+ignition::math::Pose3d g_target_pose      {0,0,0,0,0,0};
+ignition::math::Pose3d g_hand_pose      {0,0,0,0,0,0};
+int g_grasp_idx {0};
+
 int main(int _argc, char **_argv)
 {
     // Command-line arguments
@@ -19,9 +23,23 @@ int main(int _argc, char **_argv)
     gazebo::transport::SubscriberPtr sub =
         node->Subscribe(TARGET_RESPONSE_TOPIC, onTargetResponse);
 
+    // Publish to the target plugin request topic
+    gazebo::transport::PublisherPtr hand_pub =
+        node->Advertise<grasp::msgs::Hand>(HAND_REQUEST_TOPIC, 100);
+    // Subscribe to the grasp target topic and link callback function
+    gazebo::transport::SubscriberPtr hand_sub =
+        node->Subscribe(HAND_RESPONSE_TOPIC, onHandResponse);
+
+    // Subscribe to the grasp target topic and link callback function
+    gazebo::transport::SubscriberPtr grasp_idx_sub =
+        node->Subscribe("~/", onGraspIdxResponse);
+
     // Wait for a subscriber to connect to this publisher
+		std::cout << "Connecting" << std::endl;
     pub->WaitForConnection();
 		std::cout << "Target publisher connected" << std::endl;
+    hand_pub->WaitForConnection();
+		std::cout << "Hand publisher connected" << std::endl;
 
     // Interface
     Interface api;
@@ -40,13 +58,18 @@ int main(int _argc, char **_argv)
         std::stringstream input_stream(line);
         std::string command = input_stream.str();
         // Change pose request
-        if (command == "hand") {
-					std::cout << "Hand pose:\n";
-					api.getPose();
-				}
-				else if (command == "target") {
+        if (command == "pose") {
+					std::string model_name = "orange_box";
+
+					getHandPose(hand_pub);
+					waitMs(200);
+
 					getTargetPose(pub);
-				} 
+					waitMs(200);
+
+					std::string out_file = + "./trial.rest.yml"; 
+					writeToYml(out_file, model_name, g_target_pose, g_hand_pose, g_grasp_idx);
+				}
 				else {
 					std::cout << "Wrong command!\n" << std::endl;
 				}
@@ -96,11 +119,75 @@ void getTargetPose(gazebo::transport::PublisherPtr pub)
     pub->Publish(msg);
 }
 
+void getHandPose(gazebo::transport::PublisherPtr pub)
+{
+    grasp::msgs::Hand msg;
+    msg.set_type(grasp::msgs::Hand::GET);
+    pub->Publish(msg);
+}
+
 void onTargetResponse(TargetResponsePtr & _msg)
 {
     if (_msg->has_pose())
     {
         ignition::math::Pose3d pose(gazebo::msgs::ConvertIgn(_msg->pose()));
+				g_target_pose = pose;
         std::cout << "\nReceived target pose " << pose << std::endl;
     }
+}
+
+void onHandResponse(HandMsgPtr & _msg)
+{
+    if (_msg->has_pose())
+    {
+        ignition::math::Pose3d pose(gazebo::msgs::ConvertIgn(_msg->pose()));
+				g_hand_pose = pose;
+        std::cout << "\nReceived hand pose " << pose << std::endl;
+    }
+}
+
+void onGraspIdxResponse(IntMsgPtr & _msg) 
+{
+		g_grasp_idx = _msg->data();
+		std::cout << "\nReceived grasp idx " << g_grasp_idx << std::endl;
+}
+
+void writeToYml(
+    const std::string & file_name,
+    const std::string & object_name,
+    const ignition::math::Pose3d & target_pose,
+    const ignition::math::Pose3d & hand_pose,
+		const int grasp_idx)
+{
+    YAML::Node root;
+
+		// Build YAML node
+		root[object_name]["pose"][0] = target_pose.Pos().X();
+		root[object_name]["pose"][1] = target_pose.Pos().Y();
+		root[object_name]["pose"][2] = target_pose.Pos().Z();
+		root[object_name]["pose"][3] = target_pose.Rot().Roll();
+		root[object_name]["pose"][4] = target_pose.Rot().Pitch();
+		root[object_name]["pose"][5] = target_pose.Rot().Yaw();
+
+		root["hand"]["pose"][0] = hand_pose.Pos().X();
+		root["hand"]["pose"][1] = hand_pose.Pos().Y();
+		root["hand"]["pose"][2] = hand_pose.Pos().Z();
+		root["hand"]["pose"][3] = hand_pose.Rot().Roll();
+		root["hand"]["pose"][4] = hand_pose.Rot().Pitch();
+		root["hand"]["pose"][5] = hand_pose.Rot().Yaw();
+
+		root["grasp index"][0] = grasp_idx;
+
+    std::ofstream fout(file_name);
+    if (!fout) {
+        errorPrintTrace("Could not write to " << file_name);
+    } else {
+        fout << root;
+        debugPrintTrace("All rest poses written to " << file_name);
+    }
+}
+
+void inline waitMs(int delay)
+{
+    std::this_thread::sleep_for(std::chrono::milliseconds(delay));
 }
--- examples/record_poses.hh
+++ examples/record_poses.hh
@@ -21,6 +21,11 @@
 #define TARGET_REQUEST_TOPIC   "~/grasp/target"
 /// Topic to which target plugin publishes replies
 #define TARGET_RESPONSE_TOPIC  "~/grasp/target/response"
+//
+/// Topic monitored by target plugin for incoming requests
+#define HAND_REQUEST_TOPIC   "~/hand"
+/// Topic to which target plugin publishes replies
+#define HAND_RESPONSE_TOPIC  "~/hand/response"
 
 /// Get pose request
 #define TARGET_GET_POSE        grasp::msgs::TargetRequest::GET_POSE
@@ -33,15 +38,45 @@
 typedef const boost::shared_ptr<const grasp::msgs::TargetResponse>
     TargetResponsePtr;
 
-/// \brief Requests updated target object resting pose
-/// \param pub Publisher to target plugin topic
-void getTargetPose(gazebo::transport::PublisherPtr pub);
+/// Shared pointer declaration for hand message type
+typedef const boost::shared_ptr<const grasp::msgs::Hand>
+    HandMsgPtr;
+
+/// Shared pointer declaration for hand message type
+typedef const boost::shared_ptr<const gazebo::msgs::Int>
+    IntMsgPtr;
 
 /// TODO
-void onTargetResponse(TargetResponsePtr & _msg);
-
 void parseArgs(
     int argc,
     char** argv,
     std::string & cfg_dir,
     std::string & robot);
+
+/// \brief Requests updated target object resting pose
+/// \param pub Publisher to target plugin topic
+void getTargetPose(gazebo::transport::PublisherPtr pub);
+
+/// TODO
+void getHandPose(gazebo::transport::PublisherPtr pub);
+
+/// TODO
+void onTargetResponse(TargetResponsePtr & _msg);
+
+/// TODO
+void onHandResponse(HandMsgPtr & _msg);
+
+/// TODO
+void onGraspIdxResponse(IntMsgPtr & _msg);
+
+/// TODO
+void writeToYml(
+    const std::string & file_name,
+    const std::string & object_name,
+    const ignition::math::Pose3d & target_pose,
+    const ignition::math::Pose3d & hand_pose,
+		const int grasp_idx);
+
+/// TODO
+void inline waitMs(int delay);
+
